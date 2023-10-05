c85a1aad4ee833f4062a9478fc21477f0e10f803
iliask796 <iliaskal7@gmail.com>
2023-10-05 11:15:48 +0300

    Initial project files (2.0)

--- src/main/java/com/booleanuk/requests/Main.java
+++ src/main/java/com/booleanuk/requests/Main.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RequestsApplication {
-
+public class Main {
 	public static void main(String[] args) {
-		SpringApplication.run(RequestsApplication.class, args);
+		SpringApplication.run(Main.class, args);
 	}
-
 }
